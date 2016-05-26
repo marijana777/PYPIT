13c2efcbdd018118709a74b72ab1fa9bdc1de06f
J. Xavier Prochaska <xavier@ucolick.org>
2016-05-26 14:35:35 -0700

    tcent tests

--- src/arholy.py
+++ src/arholy.py
@@ -1,4 +1,7 @@
 import numpy as np
+import json
+
+
 import armsgs
 import arutils
 import ararclines
@@ -12,47 +15,46 @@ except:
 msgs = armsgs.get_logger()
 
 
-def init_holy2(use_noncalib=True):
-    """ Initialize the start of holy phase 2
+def run_holy2(tcent, idpix, idwave, npix, llist, noncalib=None, ngrid=100,
+              frac_extra=0.25, p23_frac=0.25, verbose=True,
+              debug=False, close_tol=2.):
+    """ Run Holy 2
+
+    Parameters
+    ----------
+    tcent
+    idpix
+    idwave
+    llist
+    noncalib
+    frac_extra : float, optinal
+      Fraction of estimated spectral range to expand linelist matching
+    p23_frac : float, optional
+      Fraction of detector to allows shift from p2 and p3
+    close_tol : float, optional
+      If there are multiple lines within close_tol, don't ID this line
+
     Returns
     -------
 
     """
-    import json
     from scipy.optimize import curve_fit
 
-    # Open test solution
-    root = '/Users/xavier/local/Python/PYPIT'
-    wvsoln_fil = root+'/test_suite/lrisr_600_7500_holy.json'
-    with open(wvsoln_fil) as data_file:
-        wvsoln_dict = json.load(data_file)
-    xnorm = (wvsoln_dict['xnorm']-1)
-    all_idpix = np.array(wvsoln_dict['xfit'])*xnorm
-    all_idwv = np.array(wvsoln_dict['yfit'])
-
-    # Grab inner 5 lines (as our initial test)
-    #diff = np.abs(all_idpix-424)
-    #diff = np.abs(all_idpix-800)
-    diff = np.abs(all_idpix-1024)
-    asrt = np.argsort(diff)
-    idpix = all_idpix[asrt[0:5]]
-    idpix.sort()
-    idwave = all_idwv[asrt[0:5]]
-    idwave.sort()
-
-    # Fit with 3rd order
+    # Fit ID lines with 2nd order polynomial
     func = 'polynomial'
     mask, pparam = arutils.robust_polyfit(idwave, idpix, 2, function=func)
+    wmask, wparam = arutils.robust_polyfit(idpix, idwave, 2, function=func)
     pixfit = arutils.func_val(pparam, idwave, func)
-    prms = np.sqrt(np.mean((pixfit-idpix)**2))
-    print('RMS = {:g}'.format(prms))
+    if verbose:
+        prms = np.sqrt(np.mean((pixfit-idpix)**2))
+        print('RMS = {:g}'.format(prms))
 
     # Setup global pixel fit
     wvmin, wvmax = np.min(idwave), np.max(idwave)
     wvcen = np.mean([wvmin,wvmax])
     pixcen = arutils.func_val(pparam, wvcen, func)
     dpixcen = pparam[1]*wvcen + pparam[2]*(wvmax**2-wvmin**2)/(wvmax-wvmin)*wvcen
-    chk = True
+    chk = False
     if chk:
         wvval = (idwave - wvcen)/wvcen
         idpval = idpix - pixcen - dpixcen*wvval
@@ -61,40 +63,41 @@ def init_holy2(use_noncalib=True):
     def x2x3_fit(x, p2, p3):
         return p2*x**2 + p3*x**3
 
+    '''
     # Right answer
     xxval = (all_idwv-wvcen)/wvcen
     yyval = all_idpix-pixcen-dpixcen*xxval
     ppopt, ppcov = curve_fit(x2x3_fit, xxval, yyval)
 
-    # Linelist
-    alist = ararclines.load_arcline_list(None,None,['ArI','NeI','HgI','KrI','XeI'],None)
-    llist = np.array(alist['wave'])
-    llist.sort()
     ends = arutils.func_val(wvsoln_dict['fitc'], np.array([0.,1.]), wvsoln_dict['function'],
                             minv=wvsoln_dict['fmin'], maxv=wvsoln_dict['fmax'])
-    keep = np.where((llist > ends[0]-200) & (llist < ends[1]+200))[0]
+    '''
+
+    # Cut down line list
+    dwv = wparam[1]
+    extra = frac_extra * npix * dwv
+    ends = arutils.func_val(wparam, np.array([0.,npix]), func)
+    keep = np.where((llist > ends[0]-extra) & (llist < ends[1]+extra))[0]
     llist = llist[keep]
 
     # Add in non-calib lines
-    tcent = np.array(wvsoln_dict['tcent'])
-    if use_noncalib:
-        msktc = tcent == tcent
-        for jj,ipix in enumerate(all_idpix):
-            diff = np.abs(tcent-ipix)
-            if np.min(diff) < 1.:
-                msktc[np.argmin(diff)] = False
-        newtc = tcent[msktc]
-        newwv = arutils.func_val(wvsoln_dict['fitc'], newtc/xnorm, wvsoln_dict['function'],
-                                 minv=wvsoln_dict['fmin'], maxv=wvsoln_dict['fmax'])
-        allwv = np.concatenate([llist,newwv])
+    if noncalib is not None:
+        allwv = np.concatenate([llist,noncalib])
     else:
         allwv = llist
 
-    # Setup
+    # Setup xval
     allwv.sort()
     xall = (allwv-wvcen)/wvcen
     xall2 = xall**2
     xall3 = xall**3
+
+    # Setup p2 and p3 limits
+    xmx = np.max(np.abs(xall))
+    p2min = -1 * p23_frac * npix / xmx**2
+    p2max = p23_frac * npix / xmx**2
+    p3min = -1 * p23_frac * npix / xmx**3
+    p3max = p23_frac * npix / xmx**3
 
     # TODO
     #  Scan from +/- npix in p2 and p3
@@ -104,8 +107,7 @@ def init_holy2(use_noncalib=True):
     pix_img = np.outer(tcent, np.ones(len(allwv)))
 
     # Ready to go
-    ntst = 100
-    '''  1D
+    '''  1D (p3 only)
     tst_val = np.linspace(0., 2*ppopt[1], ntst)
     tst_metric = np.zeros(ntst)
     wv_to_pix_setup = pixcen + dpixcen*xall + ppopt[0]*xall2
@@ -117,42 +119,56 @@ def init_holy2(use_noncalib=True):
         if (jj % 10) == 0:
             print('jj = {:d}'.format(jj))
     '''
-    tst_val2 = np.linspace(0., 2*ppopt[0], ntst)
-    tst_val3 = np.linspace(0., 2*ppopt[1], ntst)
-    tst_metric = np.zeros((ntst,ntst))
+    #tst_val2 = np.linspace(0., 2*ppopt[0], ngrid)
+    #tst_val3 = np.linspace(0., 2*ppopt[1], ngrid)
+    scan_p2 = np.linspace(p2min, p2max, ngrid)
+    scan_p3 = np.linspace(p3min, p3max, ngrid)
+    metric = np.zeros((ngrid,ngrid))
     wv_to_pix_setup = pixcen + dpixcen*xall
-    for ii in xrange(ntst):
-        for jj in xrange(ntst):
+    for ii in xrange(ngrid):
+        for jj in xrange(ngrid):
             # The next lines could/should be 'mapped'
-            wv_to_pix = wv_to_pix_setup + tst_val2[ii]*xall2 + tst_val3[jj]*xall3
+            wv_to_pix = wv_to_pix_setup + scan_p2[ii]*xall2 + scan_p3[jj]*xall3
             # Main call
-            tst_metric[ii,jj] = holy_cross_lines(pix_img, wv_to_pix)
+            metric[ii,jj] = holy_cross_lines(pix_img, wv_to_pix)
         #if (ii % 10) == 0:
         #    print('ii = {:d}'.format(ii))
-    print('max = {:g}'.format(np.max(tst_metric)))
+    if verbose:
+        print('max = {:g}'.format(np.max(metric)))
 
     # ID lines (Line list only)
     match_tol = 0.3  # Fraction of a pixel
-    min_idx = np.where(tst_metric == np.max(tst_metric))
-    match_p2 = tst_val2[min_idx[0][0]]
-    match_p3 = tst_val3[min_idx[1][0]]
+    min_idx = np.where(metric == np.max(metric))
+    match_p2 = scan_p2[min_idx[0][0]]
+    match_p3 = scan_p3[min_idx[1][0]]
+    if verbose:
+        print('p2 = {:g}'.format(match_p2))
+        print('p3 = {:g}'.format(match_p3))
     xmatch = (llist-wvcen)/wvcen
     match_pix = pixcen + dpixcen*xmatch + match_p2*xmatch**2 + match_p3*xmatch**3
     tids = np.zeros(len(tcent))
     for kk,ipix in enumerate(tcent):
         diff = np.abs(match_pix-ipix)
-        if np.min(diff) < match_tol:
+        nclose = np.sum(diff < close_tol)
+        if (np.min(diff) < match_tol) & (nclose == 1):
             tids[kk] = llist[np.argmin(diff)]
     # Test again input
+    '''
     aids = np.zeros(len(all_idpix))
     for kk,ipix in enumerate(all_idpix):
         diff = np.abs(match_pix-ipix)
         if np.min(diff) < match_tol:
             aids[kk] = llist[np.argmin(diff)]
     debugger.xpcol(all_idpix, aids, aids-all_idwv)
-    debugger.set_trace()
-    debugger.ximshow(tst_metric)
-
+    '''
+    if verbose:
+        print('NID = {:d}'.format(np.sum(tids > 1)))
+    #debugger.xpcol(tcent, tids)#, aids-all_idwv)
+    if debug:
+        debugger.set_trace()
+        debugger.ximshow(metric)
+    # Return
+    return tids
 
 
 def holy_cross_lines(pix_img, wv_to_pix, max_off=5., two_inv_sigma_sq=2./4):
@@ -182,3 +198,4 @@ def holy_cross_lines(pix_img, wv_to_pix, max_off=5., two_inv_sigma_sq=2./4):
     metric = np.sum(np.exp(-1*min_diff[keep]**2/two_inv_sigma_sq))
     # Return
     return metric
+
--- test_suite/test_holy.py
+++ test_suite/test_holy.py
@@ -1,11 +1,16 @@
 # Tests for the Holy Grail
 from __future__ import print_function, absolute_import, division, unicode_literals
+import linetools.utils as ltu
 
-import sys, os
+import sys, os, json
 import numpy as np
 from scipy.io.idl import readsav
 
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'stixgeneral'
+from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
 
 this_file = os.path.realpath(__file__)
 this_path = this_file[:this_file.rfind('/')]
@@ -22,6 +27,8 @@ msgs = msgs.get_logger((None, debug, last_updated, version, verbose))
 import ararc
 import ararclines
 import arcyarc
+import arutils
+import arholy
 
 try:
     from xastropy.xutils import xdebug as debugger
@@ -142,6 +149,299 @@ def test_lrisr_600_7500(debug=True):
                                   extrap_off=750.)
     # PDF
     debugger.set_trace()
+
+
+def init_holy2_test(infil):
+    # Open test solution
+    root = '/Users/xavier/local/Python/PYPIT'
+    wvsoln_fil = root+infil
+    with open(wvsoln_fil) as data_file:
+        wvsoln_dict = json.load(data_file)
+
+    # Setup ID lines
+    xnorm = (wvsoln_dict['xnorm']-1)
+    npix = xnorm + 1  # ASSUMED
+    all_idpix = np.array(wvsoln_dict['xfit'])*xnorm
+    all_idwv = np.array(wvsoln_dict['yfit'])
+    tcent = np.array(wvsoln_dict['tcent'])
+    print('Number of Input IDs = {:d}'.format(len(all_idwv)))
+
+    # Mask ID
+    tmsk = tcent==tcent
+    twv = np.zeros(len(tcent))
+    for ii,itc in enumerate(tcent):
+        if np.min(np.abs(itc-all_idpix)) > 0.1:
+            tmsk[ii] = False
+        else:
+            twv[ii] = all_idwv[np.argmin(np.abs(itc-all_idpix))]
+
+    # Linelist
+    alist = ararclines.load_arcline_list(None,None,['ArI','NeI','HgI','KrI','XeI'],None)
+    llist = np.array(alist['wave'])
+    llist.sort()
+
+    # Return
+    return wvsoln_dict, npix, all_idpix, all_idwv, tcent, tmsk, twv, llist
+
+def generate_noncalib(tcent, all_idpix, wvsoln_dict):
+    """ Generate a list of noncalib parameters
+    Parameters
+    ----------
+    tcent
+    all_idpix
+    wvsoln_dict
+
+    Returns
+    -------
+
+    """
+    msktc = tcent == tcent
+    for jj,ipix in enumerate(all_idpix):
+        diff = np.abs(tcent-ipix)
+        if np.min(diff) < 1.:
+            msktc[np.argmin(diff)] = False
+    newtc = tcent[msktc]
+    newwv = arutils.func_val(wvsoln_dict['fitc'], newtc/(wvsoln_dict['xnorm']-1),
+                             wvsoln_dict['function'],
+                             minv=wvsoln_dict['fmin'], maxv=wvsoln_dict['fmax'])
+    # Return
+    return newwv
+
+def grab_id_lines(all_idpix, all_idwv, icen, nline):
+    """
+    Parameters
+    ----------
+    all_idpix
+    all_idwv
+    icen
+    nline
+
+    Returns
+    -------
+
+    """
+    diff = np.abs(all_idpix-icen)
+    asrt = np.argsort(diff)
+    idpix = all_idpix[asrt[0:nline]]
+    idpix.sort()
+    idwave = all_idwv[asrt[0:nline]]
+    idwave.sort()
+    # Return
+    return idpix, idwave
+
+def evalaute_ids(tids, tmsk, twv):
+    """
+    Parameters
+    ----------
+    tids
+    tmsk
+    twv
+
+    Returns
+    -------
+
+    """
+    ID = tids[tmsk] > 1.
+    gdID = np.abs(tids[tmsk]-twv[tmsk]) < 0.1
+    badID = (tids[tmsk] > 1.) & (np.abs(tids[tmsk]-twv[tmsk]) > 0.1)
+    #
+    return ID, gdID, badID
+
+
+def test_holy1(infil='/test_suite/lrisr_600_7500_holy.json',
+               outfil=None, ngrid=250, p23_frac = 0.25):
+    """ Test number and location of Holy 1 lines
+
+    Parameters
+    ----------
+    infil : str, optional
+      JSON file containing the good fit
+
+    Returns
+    -------
+
+    """
+    # Init
+    wvsoln_dict, npix, all_idpix, all_idwv, tcent, tmsk, twv, llist = init_holy2_test(infil)
+
+    # Loop on nlines and pixcen
+    nlines = [3,4,5]
+    pixcen = np.round(np.linspace(100.,2000.,10)).astype(int)
+    odict = dict(ngrid=ngrid, p23_frac=p23_frac, nlines=nlines,
+                    pixcen=pixcen, runs={}, maxID=np.sum(tmsk))
+    for nline in nlines:
+        print('nline = {:d}'.format(nline))
+        for icen in pixcen:
+            idpix,idwave = grab_id_lines(all_idpix, all_idwv, icen, nline)
+            # Run
+            tids = arholy.run_holy2(tcent, idpix, idwave, npix, llist,
+                             p23_frac=p23_frac, ngrid=ngrid, verbose=False)
+            # Evaluate
+            ID, gdID, badID = evalaute_ids(tids, tmsk, twv)
+            # Fill
+            key = '{:d}_{:d}'.format(nline,icen)
+            odict['runs'][key] = {}
+            odict['runs'][key]['NID'] = np.sum(ID)
+            odict['runs'][key]['NGOOD'] = np.sum(gdID)
+            odict['runs'][key]['NBAD'] = np.sum(badID)
+    # Write?
+    if outfil is not None:
+        gddict = ltu.jsonify(odict)
+        ltu.savejson(outfil, gddict, overwrite=True)
+        print("Wrote output to {:s}".format(outfil))
+
+
+def test_tcent(infil='/test_suite/lrisr_600_7500_holy.json',
+               outfil=None, ngrid=250, p23_frac = 0.25,
+               nmodify=None,
+               seed=1234):
+    """  Test adding false tcent or removing real ones
+    Parameters
+    ----------
+    infil
+    outfil
+    ngrid
+    p23_frac
+
+    Returns
+    -------
+
+    """
+    # Init
+    wvsoln_dict, npix, all_idpix, all_idwv, tcent, tmsk, twv, llist = init_holy2_test(infil)
+    pixcen = 1024
+    nline = 4
+    ntrial = 2
+    ntcent = len(tcent)
+    rstate = np.random.RandomState(seed)
+
+    # Loop on modifying tcent
+    if nmodify is None:
+        nmodify = [-20, 40]
+        #nmodify = [-20, -10, -5, 0, 5, 10, 20, 40]
+    pixcen = np.round(np.linspace(100., 2000., 3)).astype(int)
+    #pixcen = np.round(np.linspace(100., 2000., 5)).astype(int)
+    odict = dict(ngrid=ngrid, p23_frac=p23_frac, nlines=nline,
+                    pixcen=pixcen, nmodify=nmodify, ntrial=ntrial, runs={})
+    for nmod in nmodify:
+        print('nmodify = {:d}'.format(nmod))
+        for icen in pixcen:
+            idpix, idwave = grab_id_lines(all_idpix, all_idwv, icen, nline)
+            # Trials
+            for qq in xrange(ntrial):
+                if nmod < 0:
+                    msk = tcent == tcent
+                    rand = rstate.random_sample(ntcent)
+                    srt = np.argsort(rand)
+                    msk[srt[0:-1*nmod]] = False
+                    mtcent = tcent[msk]
+                    mtmsk = tmsk[msk]
+                    mtwv = twv[msk]
+                elif nmod > 0:
+                    newtc = npix*rstate.random_sample(nmod)
+                    mtcent = np.concatenate([tcent,newtc])
+                    mtmsk = np.concatenate([tmsk, np.array([False]*nmod)])
+                    mtwv = np.concatenate([twv, np.zeros(nmod)])
+                # Run
+                tids = arholy.run_holy2(mtcent, idpix, idwave, npix, llist,
+                                 p23_frac=p23_frac, ngrid=ngrid, verbose=False)
+                # Evaluate
+                ID, gdID, badID = evalaute_ids(tids, mtmsk, mtwv)
+                # Fill
+                key = '{:d}_{:d}_{:d}'.format(icen, nmod, qq)
+                odict['runs'][key] = {}
+                odict['runs'][key]['NID'] = np.sum(ID)
+                odict['runs'][key]['NGOOD'] = np.sum(gdID)
+                odict['runs'][key]['NBAD'] = np.sum(badID)
+                odict['runs'][key]['maxID'] = np.sum(mtmsk)
+    # Write?
+    if outfil is not None:
+        gddict = ltu.jsonify(odict)
+        ltu.savejson(outfil, gddict, overwrite=True)
+        print("Wrote output to {:s}".format(outfil))
+
+
+""" PLOTS """
+def plot_ngoodbad(json_fil, parms, title, outfil):
+    """
+    Parameters
+    ----------
+    json_fil
+    title
+    xlbl
+    outfil
+
+    Returns
+    -------
+
+    """
+    odict = ltu.loadjson(json_fil)
+    all_runs = odict['runs'].keys()
+    nruns = len(all_runs)
+    # Parameters
+    nparm = len(parms)
+    if nparm != len(all_runs[0].split('_')):
+        raise ValueError("Parameter coding is not as expected!")
+    if nparm > 3:
+        raise ValueError("Not ready for this")
+
+    # Start the plot
+    pp = PdfPages(outfil)
+
+    plt.figure(figsize=(5, 5))
+    plt.clf()
+    gs = gridspec.GridSpec(1, 1)
+    ax = plt.subplot(gs[0])
+    lsz = 15.
+    ax.set_ylabel('N_GOOD', fontsize=lsz)
+
+    ax2 = ax.twinx()
+    ax2.set_ylabel('N_BAD', fontsize=lsz)
+
+    # Nparam
+    symbols  = ['o', 's', 'v', 'x', '^']
+    if nparm >= 2:
+        # Points use parm0, x is from parm1
+        # Parse runs
+        parm0 = np.zeros(nruns).astype(int)
+        parm1 = np.zeros(nruns).astype(int)
+        ngood = np.zeros(nruns)
+        nbad = np.zeros(nruns)
+        for kk,run in enumerate(all_runs):
+            vals = run.split('_')
+            parm0[kk] = int(vals[0])
+            parm1[kk] = int(vals[1])
+            ngood[kk] = odict['runs'][run]['NGOOD']
+            nbad[kk] = odict['runs'][run]['NBAD']
+        for ss, iparm0 in enumerate(odict[parms[0]]):
+            gd0 = np.where(parm0 == iparm0)[0]
+            debugger.set_trace()
+            ax.scatter(parm1[gd0], ngood[gd0], marker=symbols[ss], color='blue',
+                       label='{}'.format(iparm0))
+            ax2.scatter(parm1[gd0], nbad[gd0], marker=symbols[ss], color='red')
+
+    ax.set_xlabel(parms[1], size=lsz)
+
+    # Legend
+    #legend = ax.legend(loc='lower left', borderpad=0.3,
+    #                   handletextpad=0.3, fontsize='large')
+    legend = ax.legend(loc='lower left', scatterpoints=1, borderpad=0.3,
+                        handletextpad=0.3, fontsize='small', numpoints=1)
+
+    # Label
+    #ax.text(0.05, 0.87, lbl, transform=ax.transAxes, color='black', size=csz, ha='left')
+    #lbl2 = '({:d} systems)'.format(nsys)
+    #ax.text(0.05, 0.75, lbl2, transform=ax.transAxes, color='black', size=csz, ha='left')
+    # Axes
+    #ax.xaxis.set_major_locator(plt.MultipleLocator(1.))
+
+    plt.title(title, fontsize=20)
+    # End
+    print('Writing {:s}'.format(outfil))
+    plt.tight_layout(pad=0.2,h_pad=0.,w_pad=0.1)
+    pp.savefig()
+    pp.close()
+    plt.close()
 
 
 
