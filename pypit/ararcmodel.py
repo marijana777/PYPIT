75343290b8937cc766a1915592f00ab66e732322
J. Xavier Prochaska <xavier@ucolick.org>
2016-12-16 07:31:19 -0800

    some success

--- pypit/ararcmodel.py
+++ pypit/ararcmodel.py
@@ -27,8 +27,9 @@ msgs = armsgs.get_logger()
 import pypit
 model_path = pypit.__path__[0]+'/data/arc_lines/Model/'
 
+
 def add_to_lists(tbl):
-    """ Add to existing lamp lists
+    """ Add to existing lamp lists (or generate a new one)
     EXPERTS ONLY
 
     Parameters
@@ -83,7 +84,52 @@ def load_lamps(lamps):
     return arc_tbl
 
 
-def model_arcspec(lamps, wvcen, deltawv, npix, stretch, fwhm):
+def match_quad_to_list(spec_lines, line_list, wv_guess, dwv_guess,
+                  tol=2., dwv_uncertainty=0.2):
+    """
+    Parameters
+    ----------
+    spec_lines : ndarray
+      pixel space
+    line_list
+    tol
+
+    Returns
+    -------
+    possible_matchtes : list
+      list of indices of matching quads
+
+    """
+    # Setup spec
+    npix = spec_lines[-1]-spec_lines[0]
+    spec_values = (spec_lines[1:-1]-spec_lines[0])/(
+        spec_lines[-1]-spec_lines[0])
+    ftol = tol/npix
+    #
+    possible_start = np.where((line_list > wv_guess[0]) & (line_list < wv_guess[1]))[0]
+    possible_matches = []
+    for start in possible_start:
+        #print("Trying {:g}".format(line_list[start]))
+        # Find possible ends
+        possible_ends = np.where( (line_list > line_list[start] + npix*dwv_guess*(1-dwv_uncertainty)) &
+                                     (line_list < line_list[start] + npix*dwv_guess*(1+dwv_uncertainty)))[0]
+        # Loop on ends
+        for end in possible_ends:
+            values = (line_list[start+1:end]-line_list[start]) / (
+                line_list[end]-line_list[start])
+            # Test
+            tst0 = np.abs(values-spec_values[0]) < ftol
+            tst1 = np.abs(values-spec_values[1]) < ftol
+            #if np.abs(line_list[start]-6097.8) < 0.2:
+            #    debugger.set_trace()
+            if np.any(tst0) & np.any(tst1):
+                possible_matches.append([start, start+1+np.where(tst0)[0][0],
+                                       start+1+np.where(tst1)[0][0], end])
+    # Return
+    return possible_matches
+
+def model_arcspec(lamps, wvcen, deltawv, npix, stretch,
+                  fwhm=4., npad=100, const_kms=False):
     """
     Parameters
     ----------
@@ -93,7 +139,7 @@ def model_arcspec(lamps, wvcen, deltawv, npix, stretch, fwhm):
       Total wavelength coverage in Ang, e.g. 1500A
     npix
     stretch
-    fwhm
+    fwhm : float, optional
 
     Returns
     -------
@@ -107,7 +153,20 @@ def model_arcspec(lamps, wvcen, deltawv, npix, stretch, fwhm):
 
     # Dispersion
     dwv = deltawv/npix
+
+    # Wave to pix function (normalized?)
+    #def f_wave_pix(wave, dwv):
+
+    # Generate wavelengths
     wave = dwv*np.arange(npix) + (wvcen - dwv*npix/2)
 
-    # Generate lines
+    # PAD
+
+    wvmin, wvmax = np.min(wave), np.max(wave)
+
+    # Cut
+    model_lines = (arc_tbl['wave'] > wvmin) & (arc_tbl['wave'] < wvmax)
+    model_waves = model_lines['wave'].data[model_lines]
+
+    # Generate lines -- Should Cython the following
 
